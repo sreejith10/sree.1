037bbb1ec15f2f52e508211523594c56ba88c808
sreejith lingan <94353237+sreejith10@users.noreply.github.com>
2024-08-23 23:55:17 +0530

    Add files via upload

---
+++ javascript programing/demo.js
@@ -0,0 +1,178 @@
+// function iphone()
+// {
+//     console.log("this is iphone")
+// }
+// iphone()
+// function oppo()
+// {
+//     console.log("this is oppo")
+// }
+// oppo()
+// function redmi()
+// {
+//     console.log("this is redmi")
+// }
+// redmi()
+// var a=10
+// var b=20
+// function add()
+// {
+//     console.log(a+b)
+// }
+// add()
+
+// var factor="kamal"
+// var fplayer="dhoni"
+// var fmovie="after"
+// function favorite()
+// {
+//     console.log("favorite actor:"+factor)
+//     console.log("favorite player:"+fplayer)
+//     console.log("favorite movie:"+fmovie)
+
+// }
+//  favorite()
+
+
+
+//  function add(a,b)
+//  {
+// console.log(a+b)
+
+//  }
+// add()
+
+// function area(l,b)
+// {
+//     var a=l*b
+//     console.log("area is:"+a)
+// } 
+// var length=10
+// var breadth=20
+// area (10,20)
+
+
+// function myname()
+// {
+//     return "sreejith lingan"
+// }
+// var a=myname()
+// console.log(a)
+
+// function add(a,b)
+// {
+//     return (a+b)
+// }
+// var total=add(10,20)
+
+// console.log(total)
+
+
+// var brush =false
+// if(brush)
+// {
+//     console.log("your teeth are clean")
+// }
+// else
+// {
+//     console.log("go brush your teeth now")
+// }/
+
+// var birthday=false
+// if(birthday)
+// {
+//     console.log("happy birthday! enjoy ur special day")
+// }
+// else
+// {
+//     console.log("have a nice day")
+// }
+
+// console.log(false&&false)
+// console.log(false||false)
+// console.log(!false)
+
+// var color="green"
+// if(color== "red")
+// {
+//     console.log("stop")
+// }
+// if (color=="yellow")
+// {
+//     console.log("get ready")
+// }
+// if (color=="green")
+// {
+//     console.log("go")
+// }
+
+// var season="winter"
+// if(season=="spring")
+// {
+//     console.log("Enjoy the blooming flowers.")
+// }
+// if(season=="summer")
+// {
+//     console.log("have fun in the sun")
+// }
+// if(season=="autumn")
+// {
+//     console.log("admire the color full leaves")
+// }
+// if(season=="winter")
+// {
+//     console.log("bundel up and stay warm")
+// }
+
+
+// var score= 69
+// if(score<=50)
+// {
+//  console.log("you need to improve")
+// }
+// else if(score >50&& score <=70)
+// {
+//     console.log("good job")
+// }
+// else if (score>70)
+// {
+//     console.log("Exclent performance!")
+// }
+
+// var num = 2
+// if (num %2 ==0)
+// {
+//     console.log("the num is even")
+
+// }
+// else if(num %2 !=0)
+// {
+//     console.log("the num is odd")
+// }
+// var i=2   
+// for i<5
+//     {console.log("hello")}
+
+// for(i=10;i>=1;i=i-1)
+// {
+   
+//     console.log(i+i*2)
+// }
+// Javascript program to print
+// table of a number
+
+// var n = 5;
+// for (let i = 1; i <= 10; ++i)
+//     console.log(n + " * " + i +
+//         " = " + n * i);
+
+       
+          
+
+// for(i=1;i<=20;i++)
+// {
+//     if(i%2==0)
+//     console.log(i)
+// }
+
+                                                                                                                    
